4835745682f92cb12d22f04311dc6bb50da0815f
Cindy Kandie <37507239+cindykandie@users.noreply.github.com>
2020-09-15 10:33:24 +0300

    Add files via upload

---
+++ msg.js
@@ -0,0 +1,8 @@
+// let submitButton = document.querySelector('button')
+// let writtenMessage = document.querySelector('.type-here')
+// let passedMessage = document.querySelector('#the-message')
+
+// submitButton.addEventListener('click', passMessage)
+// function passMessage(){
+//     console.log('dits')
+// }
