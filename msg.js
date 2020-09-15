25501eaeed7e5717c3210445c53c5660fb645232
Cindy Kandie <37507239+cindykandie@users.noreply.github.com>
2020-09-15 11:56:49 +0300

    Delete msg.js

--- msg.js
+++
@@ -1,8 +0,0 @@
-// let submitButton = document.querySelector('button')
-// let writtenMessage = document.querySelector('.type-here')
-// let passedMessage = document.querySelector('#the-message')
-
-// submitButton.addEventListener('click', passMessage)
-// function passMessage(){
-//     console.log('dits')
-// }
